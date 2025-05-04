1e4b7773092786175de7e239b8f1b6d5cc678066
kangjiungdev <kangjiungdev@gmail.com>
2025-05-05 07:30:25 +0900

    그냥 피보나치 수열

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use std::io::stdin;
+use std::io::{Write, stdin, stdout};
 
 fn main() {
     loop {
         let mut input_value = String::new();
+        print!("피보나치 수열: ");
+        stdout().flush().unwrap();
         let success = stdin().read_line(&mut input_value);
         if success.is_err() {
             println!("에러: 입력 값 받을 수 없음");
@@ -13,28 +15,19 @@ fn main() {
         let input_value = match success {
             Ok(value) => value,
             Err(_) => {
-                println!("에러: 입력 값 파싱 불가");
+                println!("에러: 입력 값 파싱 불가\n");
                 continue;
             }
         };
-        let mut a = 0; // 첫번째 수
-        let mut b = 0; // 두번째 수
-        let mut c: u8 = 0; // a 구해야 하는지 b 구해야 하는지 판단. a 구할 땐 0, b 구할 땐 1.
-        for _ in 1..=input_value {
-            if a == 0 {
-                // 0으로 시작.
-                b = 1;
-            }
-            if c == 0 {
-                a += b;
-                c += 1;
-            } else {
-                b += a;
-                c -= 1;
-            }
-        }
-        let result = if input_value % 2 == 0 { a } else { b };
+        let result = a(input_value);
         println!("result: {result}");
         break;
     }
+}
+
+fn a(number: usize) -> usize {
+    if number == 0 || number == 1 {
+        return number;
+    }
+    a(number - 1) + a(number - 2)
 }
