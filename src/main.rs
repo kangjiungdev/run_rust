bb10817d73b3c2d341ae84faf8d8c56e2729ba74
kangjiungdev <kangjiungdev@gmail.com>
2025-05-06 03:49:00 +0900

    'The Twelve Days of Christmas' 노래 출력

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,76 @@
-use std::io::{Write, stdin, stdout};
+use std::io::{self, Write};
+
+const NEXT_LINE: &str = "\n-----------------------------------\n";
+struct Lyric {
+    lyric: String,
+    day: String,
+}
 
 fn main() {
-    loop {
-        let mut input_value = String::new();
-        print!("피보나치 수열: ");
-        stdout().flush().unwrap();
-        let success = stdin().read_line(&mut input_value);
+    let verse = loop {
+        print!("몇 절까지 출력할지 1~12 사이의 숫자를 입력하세요: ");
+        io::stdout().flush().unwrap();
+        let mut verse_number = String::new();
+        let success = io::stdin().read_line(&mut verse_number);
         if success.is_err() {
-            println!("에러: 입력 값 받을 수 없음");
-            return;
+            continue;
+        }
+        let success = verse_number.trim().parse::<u8>();
+        if success.is_err() {
+            continue;
+        }
+        break success.unwrap();
+    };
+    print_lyrics(verse);
+}
+
+fn print_lyrics(verse: u8) {
+    let lyrics: [Lyric; 12] = [
+        create_lyric("A partridge in a pear tree.", "first"),
+        create_lyric("Two turtle doves", "second"),
+        create_lyric("Three French hens", "third"),
+        create_lyric("Four calling birds", "fourth"),
+        create_lyric("Five golden rings", "fifth"),
+        create_lyric("Six geese a-laying", "sixth"),
+        create_lyric("Seven swans a-swimming", "seventh"),
+        create_lyric("Eight maids a-milking", "eighth"),
+        create_lyric("Nine ladies dancing", "ninth"),
+        create_lyric("Ten lords a-leaping", "tenth"),
+        create_lyric("Eleven pipers piping", "eleventh"),
+        create_lyric("Twelve drummers drumming", "twelfth"),
+    ];
+
+    println!("{NEXT_LINE}");
+
+    let mut i = 0;
+    while (i as u8) < verse {
+        let day = &lyrics[i].day;
+        let first_lyric = format!("On the {day} day of Christmas\nMy true love gave to me");
+        let mut a = vec![];
+        for lyric in lyrics[..i + 1].iter().rev() {
+            a.push(lyric.lyric.clone());
+        }
+        if !a.is_empty() {
+            let index = a.len() - 1;
+            a[index] = String::from("And a partridge in a pear tree.");
         }
 
-        let success = input_value.trim().parse::<usize>();
-        let input_value = match success {
-            Ok(value) => value,
-            Err(_) => {
-                println!("에러: 입력 값 파싱 불가\n");
-                continue;
-            }
-        };
-        let result = a(input_value);
-        println!("result: {result}");
-        break;
+        println!("{first_lyric}");
+        for el in a {
+            println!("{el}");
+        }
+        println!("{NEXT_LINE}");
+        i += 1;
     }
 }
 
-fn a(number: usize) -> usize {
-    if number == 0 || number == 1 {
-        return number;
+fn create_string(text: &str) -> String {
+    String::from(text)
+}
+
+fn create_lyric(lyric: &str, day: &str) -> Lyric {
+    Lyric {
+        lyric: create_string(lyric),
+        day: create_string(day),
     }
-    a(number - 1) + a(number - 2)
 }
